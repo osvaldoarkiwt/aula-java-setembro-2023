279c4f19c010dda12a8b0713d4ff463b41cdd8cb
osvaldo souza <osvaldosouzaiwt@gmail.com>
2024-01-25 20:53:50 -0300

    fix - mudando o nome do package

--- orientacaoaobjeto/src/orientacaoaobjeto/alunooutronome/Aluno.java
+++ orientacaoaobjeto/src/orientacaoaobjeto/alunooutronome/Aluno.java
@@ -1,4 +1,4 @@
-package orientacaoaobjeto.alunocego;
+package orientacaoaobjeto.alunooutronome;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
--- orientacaoaobjeto/src/orientacaoaobjeto/exerciciorevisao/TesteBanco.java
+++ orientacaoaobjeto/src/orientacaoaobjeto/exerciciorevisao/TesteBanco.java
@@ -4,10 +4,14 @@ public class TesteBanco {
 
 	public static void main(String[] args) {
 		
+		ContaEspecial c1 = (ContaEspecial) new ContaCorrente(150.5);
+		
 		ContaEspecial c = new ContaEspecial(100.0,300.0);
 		
 		Cliente cliente = new Cliente("José das Tapioca");
 
+		c1.verSaldo();
+		
 		cliente.setConta(c);
 		
 		cliente.getConta().sacar(200.0);
