43dee62173272d8b3a4dd4ede2f475d9cb361717
osvaldo souza <osvaldosouzaiwt@gmail.com>
2024-05-09 21:28:38 -0300

    feat - finalizando os filtros

--- rest/src/main/java/com/projeto/rest/domain/model/filter/AlunoFilter.java
+++ rest/src/main/java/com/projeto/rest/domain/model/filter/AlunoFilter.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AlunoFilter {
-
+	private Long id;
 	private String nome;
+	private String endereco;
 }
--- rest/src/main/java/com/projeto/rest/domain/repository/spec/AlunoSpec.java
+++ rest/src/main/java/com/projeto/rest/domain/repository/spec/AlunoSpec.java
@@ -16,8 +16,16 @@ public class AlunoSpec {
 		return (root,query,builder) ->{
 			var predicates = new ArrayList<Predicate>();
 
+	         if(filtro.getId() != null) {
+	             predicates.add(builder.equal(root.get("id"), filtro.getId()));
+	         }
+			
 	         if(filtro.getNome() != null) {
 	             predicates.add(builder.like(builder.lower(root.get("nome")), "%" + filtro.getNome().toLowerCase() + "%"));
+	         }
+	         
+	         if(filtro.getEndereco() != null) {
+	             predicates.add(builder.like(builder.lower(root.get("endereco")), "%" + filtro.getEndereco().toLowerCase() + "%"));
 	         }
 	         	         
 			return builder.and(predicates.toArray(new jakarta.persistence.criteria.Predicate[0]));
